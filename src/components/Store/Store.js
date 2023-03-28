c9ef753949648cf4ba01a0676e1d5f2b029a9134
Nitin Kumar <113196521+Nitin-Baslay@users.noreply.github.com>
2023-03-28 12:08:37 +0530

    Delete src directory

--- src/components/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Slice from "./Slice";
-import Idslice from "./Idslice";
-
-const Store = configureStore({
-  reducer: { slice: Slice, id: Idslice },
-});
-export default Store;
